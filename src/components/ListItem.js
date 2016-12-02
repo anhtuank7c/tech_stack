60a574ab4e503077250cc5d1901034608292f9da
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2016-12-02 10:12:00 +0700

    Add eslint to project

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { title } = styles;
     return (
-      <CardSection>
-        <Text style={title}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectedLibraryId(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={title}>{this.props.library.title}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
+
 const styles = {
   title: {
     fontSize: 18,
     paddingLeft: 15,
   }
 }
-export default ListItem;
+
+export default connect(null, actions)(ListItem);
